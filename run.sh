91f78efa2af0b6ff2ba7b23cce190c8c8b066772
Klaus Frank <git@frank.fyi>
2022-09-09 21:22:11 +0000

    Update runscript and add new git_sync

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Init
 if [ -z $TMUX ]
 then
-    tmux new-session -d './run.sh' \; attach
+    tmux new-session './run.sh'
     exit $?
 fi
 python -m venv venv --copies
@@ -16,7 +16,7 @@ fi
 pip install -r requirements.txt
 
 # Actions
-tmux split-window -d "./docker_images.py"
-tmux split-window -d "./git_sync.py"
+tmux split-window -d "./docker_images.py; read -p 'Press any key to continue ...'"
+tmux split-window -d "./git_sync.py; read -p 'Press any key to continue ...'"
 
 # Cleanup
